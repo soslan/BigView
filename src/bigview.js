839e286daa87866c86e7f808b9d539aaf7ba3de0
Soslan Khubulov <soslanx@gmail.com>
2015-10-27 10:50:37 +0300

    Basic gallery show/hide functionaliyu

--- src/bigview.js
+++ src/bigview.js
@@ -104,6 +104,17 @@ function BigView(){
     }
   });
 
+  this.galleryButton = e({
+    class:'bv-button bv-gallery-button bv-toggle-button bv-on',
+    tag:'button',
+    parent:this.navigationRight,
+    content:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z"/></svg>',
+    action: function(e){
+      self.container.classList.toggle("bv-no-gallery");
+      self.galleryButton.classList.toggle("bv-on");
+    }
+  });
+
   this.gallery = e({
     tag:'div',
     class:'bv-gallery',
